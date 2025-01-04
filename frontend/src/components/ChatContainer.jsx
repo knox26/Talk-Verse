d6ae6ece5f64cac37094482f7efd1d576e52ebed
knox26 <bvjadhav7558@gmail.com>
2025-01-05 01:27:07 +0530

    added image download functionality

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useChatStore } from "../store/useChatStore.js";
-import { use } from "react";
 import ChatHeader from "./Chatheader.jsx";
 import MessageInput from "./MessageInput.jsx";
 import MessageSkeleton from "./skeletons/MessageSkeleton.jsx";
 import { useAuthStore } from "../store/useAuthStore.js";
 import { formatMessageTime } from "../lib/utils.js";
+import { ArrowDownToLine, X } from "lucide-react";
+import axios from "axios";
 
 function ChatContainer() {
   const {
@@ -18,6 +19,9 @@ function ChatContainer() {
   } = useChatStore();
   const { authUser } = useAuthStore();
   const messagesEndRef = useRef(null);
+  const [openImage, setOpenImage] = useState(false);
+  const [image, setImage] = useState(null);
+  const [isDownloading, setIsDownloading] = useState(false);
 
   useEffect(() => {
     if (selectedUser) {
@@ -35,6 +39,28 @@ function ChatContainer() {
     }
   }, [messages]);
 
+  const downloadImage = async (url) => {
+    setIsDownloading(true);
+
+    try {
+      const response = await axios.get(url, {
+        responseType: "blob",
+      });
+      const urlBlob = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement("a");
+      link.href = urlBlob;
+      link.setAttribute("download", url.split("/").pop()); // Use the file name from the URL
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+      window.URL.revokeObjectURL(urlBlob);
+    } catch (error) {
+      console.error("File download failed:", error);
+    } finally {
+      setIsDownloading(false);
+    }
+  };
+
   if (isMessagesLoading)
     return (
       <div className="flex-1 flex flex-col overflow-auto">
@@ -45,7 +71,7 @@ function ChatContainer() {
     );
 
   return (
-    <div className="flex-1 flex flex-col overflow-auto ">
+    <div className="relative flex-1 flex flex-col overflow-auto ">
       <ChatHeader />
       <div className="flex-1 overflow-y-auto p-4 space-y-4">
         {messages.map((message) => (
@@ -75,6 +101,10 @@ function ChatContainer() {
                   src={message.image}
                   alt="Attachment"
                   className="sm:max-w-[200px] rounded-md mb-2"
+                  onClick={() => {
+                    setOpenImage(true);
+                    setImage(message.image);
+                  }}
                 />
               )}
               {message.text && <p>{message.text}</p>}
@@ -87,6 +117,35 @@ function ChatContainer() {
           </div>
         ))}
       </div>
+      {openImage && (
+        <div className=" absolute h-full w-full z-10 backdrop-blur-lg ">
+          <div className="flex justify-center w-full gap-3 mt-5">
+            <button
+              className="p-3 rounded-full bg-zinc-800/80"
+              onClick={() => {
+                downloadImage(image);
+              }}
+            >
+              <ArrowDownToLine />
+            </button>
+            <button
+              className="p-3 rounded-full bg-zinc-800/80"
+              onClick={() => {
+                setOpenImage(false);
+              }}
+            >
+              <X />
+            </button>
+          </div>
+          <div className="w-full px-1 py-10 sm:p-10   flex justify-center items-center ">
+            <img
+              src={image}
+              alt="Preview"
+              className="object-contain h-[60vh] rounded-md shadow-lg"
+            />
+          </div>
+        </div>
+      )}
       <MessageInput />
     </div>
   );
